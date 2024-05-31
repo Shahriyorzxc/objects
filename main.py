9daefefbb0e06522183cb88b56feab1851bb32a0
Shahriyor <zxcnanevermore@gmail.com>
2024-05-31 19:21:37 +0500

    done objects

--- main.py
+++ main.py
@@ -112,15 +112,29 @@
 #
 
 # Task7
-# def letters(s):
-# #
-#
-#
-#
-#
-# print()
-# print()
-# print()
+def profit(l):
+    result = 0
+    for x,y in l.items():
+        if type(y) == int or float:
+            result += y
+    return result
+
+
+print(profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+}))
+print(profit({
+  "cost_price": 225.89,
+  "sell_price": 550.00,
+  "inventory": 100
+}))
+print(profit({
+  "cost_price": 2.77,
+  "sell_price": 7.95,
+  "inventory": 8500
+}))
 
 
 # #Task8
--- main2.py
+++ main2.py
@@ -1,18 +1,18 @@
-person = [{
-    'name': 'Ali',
-    'age': 25,
-    'friend': {
-        'name': 'Ali',
-        'age': 24,
-        'friend': {
-            'name': 'Shoxruh',
-            'age': 22,
-        }
-
-    }
-
-}]
-print(person[0]['friend']['age'] + person[0]['friend']['age'] + person[0]['friend']['age'])
+# person = [{
+#     'name': 'Ali',
+#     'age': 25,
+#     'friend': {
+#         'name': 'Ali',
+#         'age': 24,
+#         'friend': {
+#             'name': 'Shoxruh',
+#             'age': 22,
+#         }
+#
+#     }
+#
+# }]
+# print(person[0]['friend']['age'] + person[0]['friend']['age'] + person[0]['friend']['age'])
 
 
 # l = 0
@@ -33,3 +33,67 @@ print(person[0]['friend']['age'] + person[0]['friend']['age'] + person[0]['frien
 #
 # print(l)
 #
+
+
+
+#
+# l = sorted([5,2,4,1,3]), reverse=True)
+
+#
+# data = list(enumerate(["Ali", "Doni", "Vohid"]))
+# print(data)
+#
+#
+# l = ["Ali", "Vali", "Vohid", ""]
+
+
+
+
+
+
+
+# alf = ["c", "b", "a", "f", "g", "d", "e"]
+# lst = [0,1,2,3,4]
+# x = sorted(alf)
+# m = dict(zip(lst, x))
+# print(m)
+
+# dict3 = {
+#     1 : "a"
+#     # ....
+# }
+
+
+#lst = int(input("Son: "))
+# l = 0
+# for x in range(1, lst + 1):
+#     l += x
+# print(l)
+
+# l3 = 0
+# res = 0
+# while l3 == lst:
+#     res -= l3
+#     print(l3)
+#     l3 += 1
+
+string = "Shohjahon"
+for x in range(len(string)):
+    print(string[x])
+
+x = 0
+while x < len(string):
+    x += 1
+    print(x)
+
+
+t = (1,2,3,4,5,6)
+# for x in t:
+#     print(t)
+
+
+
+for x in range(0, len(t)):
+    print(t[x])
+
+
